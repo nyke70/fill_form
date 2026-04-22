8df89b43a986148697122c74d73889653118d785
Serge GABA <39250887+serge129000@users.noreply.github.com>
2026-04-22 18:10:44 +0200

    fixing dynamic port

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-echo "⏳ Attente du démarrage de MySQL..."
-while ! nc -z "$DB_HOST" "$DB_PORT"; do
-  sleep 1
-done
-echo "✅ MySQL est prêt"
+if [ -n "$DB_HOST" ] && [ -n "$DB_PORT" ]; then
+  echo "⏳ Attente du démarrage de MySQL..."
+  while ! nc -z "$DB_HOST" "$DB_PORT"; do
+    sleep 1
+  done
+  echo "✅ MySQL est prêt"
+  sleep 2
+fi
 
-sleep 2
-
-echo "🚀 Démarrage avec nodemon..."
-exec nodemon index.js
+echo "🚀 Démarrage..."
+exec node index.js
--- index.js
+++ index.js
@@ -31,6 +31,7 @@ app.post('/api/v1/submit', async (req, res) => {
 app.get('/', (req, res) => res.send('Hello World!'));
 
 // Lancement du serveur
-app.listen(3001, () => {
-  console.log(`Example app listening on port 3001`);
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
 });
